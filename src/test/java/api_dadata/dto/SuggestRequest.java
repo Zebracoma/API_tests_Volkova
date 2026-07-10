19c65f117afceffd8c1a5c6d18238bf3ed2080a1
Miray <mariya.erris@gmail.com>
2026-07-10 21:51:43 +0300

    Добавлены DTO-модели для парсинга JSON ответов DaData post-запроса

--- src/test/java/api_dadata/dto/SuggestRequest.java
+++ src/test/java/api_dadata/dto/SuggestRequest.java
@@ -1,4 +1,11 @@
 package api_dadata.dto;
 
 public class SuggestRequest {
+    private String query;
+
+    public SuggestRequest(String query) {
+        this.query = query;
+    }
+
+    public String getQuery() { return query; }
 }
--- src/test/java/api_dadata/dto/SuggestResponse.java
+++ src/test/java/api_dadata/dto/SuggestResponse.java
@@ -1,4 +1,16 @@
 package api_dadata.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class SuggestResponse {
+    private List<AddressLocation> suggestions;
+
+    public SuggestResponse() {}
+
+    public List<AddressLocation> getSuggestions(){
+        return suggestions;
+    }
 }
