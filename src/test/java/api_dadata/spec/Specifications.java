a921e90a5b546db5364cdb208e22a60f0c6529f2
Miray <mariya.erris@gmail.com>
2026-07-10 18:38:57 +0300

    Реализован успешный GET-тест определения города по IP

---
+++ src/test/java/api_dadata/spec/Specifications.java
@@ -0,0 +1,36 @@
+package api_dadata.spec;
+
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+public class Specifications {
+    private static final String API_TOKEN = "bb5fc0863d2f8be4cfe14f9fa520ba8f37e27198";
+
+    //Спецификация для отправки запроса
+    public static RequestSpecification reuestSpec (String url){
+        return new RequestSpecBuilder()
+                .setBaseUri(url)
+                .setContentType(ContentType.JSON)
+                .addHeader("Authorization", "Token " + API_TOKEN)
+                .addHeader("Accept", "application/json")
+                .build();
+    }
+
+    //Спецификация для успешного ответа
+    public static ResponseSpecification responseSpecOK200(){
+        return new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .build();
+    }
+
+    //Метод-установщик
+    public static void installSpecification(RequestSpecification request, ResponseSpecification response){
+        RestAssured.requestSpecification = request;
+        RestAssured.responseSpecification = response;
+    }
+}
--- src/test/java/api_dadata/tests/IpToLocationTest.java
+++ src/test/java/api_dadata/tests/IpToLocationTest.java
@@ -1,4 +1,34 @@
 package api_dadata.tests;
 
+import api_dadata.dto.IpResponse;
+import api_dadata.spec.Specifications;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
 public class IpToLocationTest {
+    private final String URL = "https://suggestions.dadata.ru/suggestions/api/4_1/rs";
+
+    @Test
+    public void checkCityByIpTest(){
+
+        Specifications.installSpecification(Specifications.reuestSpec(URL), Specifications.responseSpecOK200());
+        String testIp = "46.226.227.20";
+        String expectedCity = "Москва";
+
+        IpResponse resp = given()
+                .queryParam("ip", testIp)
+                .when()
+                .get("/iplocate/address")
+                .then().log().all()
+                .extract().as(IpResponse.class);
+
+        Assertions.assertNotNull(resp.getLocation(), "Локация не найдена (NULL)");
+
+        String actualCity = resp.getLocation().getData().getCity();
+
+        Assertions.assertEquals(expectedCity, actualCity, "Город определен не верно!");
+
+    }
 }
